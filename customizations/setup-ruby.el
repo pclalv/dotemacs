f88c323f44ab9e92b4650ccb268b887d1a7c3306
paul alvarez <palvarez@greenhouse.io>
2018-06-12 13:27:23 -0400

    Fix indentation in ruby files

--- customizations/setup-ruby.el
+++ customizations/setup-ruby.el
@@ -10,8 +10,13 @@
       ,(rx (or "#" "=begin"))                        ; Comment start
       ruby-forward-sexp nil)))
 
+(add-to-list 'auto-mode-alist
+             '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
+
 (global-set-key (kbd "C-c h") 'hs-hide-block)
 (global-set-key (kbd "C-c s") 'hs-show-block)
 
 (add-hook 'ruby-mode-hook 'highlight-indentation-mode)
 (add-hook 'ruby-mode-hook 'electric-pair-mode)
+
+(setq enh-ruby-deep-indent-paren nil)
