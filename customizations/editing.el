27a6bd43bc250087a956c4c68c19575390a3c45c
paul alvarez <palvarez@greenhouse.io>
2019-02-27 11:44:48 -0500

    add the vaguely useful SNES porting stuff

--- customizations/editing.el
+++ customizations/editing.el
@@ -34,3 +34,9 @@
 (global-set-key (kbd "C-x C-k") 'kill-region)
 
 (electric-pair-mode 1)
+
+;; (push '(?\< . ?\>) electric-pair-pairs) ; Automatically pair angle brackets
+
+;; i'd really like for electric pair to highlight angle brackets...
+;; (modify-syntax-entry ?< "(>")
+;; (modify-syntax-entry ?> ")<")
