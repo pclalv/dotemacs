073e2264394c10af703b0491bc420dadc20e80d1
paul alvarez <palvarez@greenhouse.io>
2019-06-02 09:50:33 -0400

    Integrate eglot with projectile

--- customizations/setup-eglot.el
+++ customizations/setup-eglot.el
@@ -7,3 +7,15 @@
 
 ;; this didn't work; cryptic error message
 ;; (add-to-list 'eglot-server-programs '(clojure-mode . ("/Users/paulalvarez/bin/clojure-lsp")))
+
+;; Bridge projectile and project together so packages that depend on
+;; project like eglot work
+;; https://github.com/joaotavora/eglot/issues/129#issuecomment-444130367
+(defun my-projectile-project-find-function (dir)
+  (let ((root (projectile-project-root dir)))
+    (and root (cons 'transient root))))
+
+(projectile-mode t)
+
+(with-eval-after-load 'project
+  (add-to-list 'project-find-functions 'my-projectile-project-find-function))
