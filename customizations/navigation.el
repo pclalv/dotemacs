f42266527c9350ee6544c2ad165b4a52bb785f4e
paul alvarez <palvarez@greenhouse.io>
2019-07-10 13:14:26 -0400

    Fully refactor helm stuff

--- customizations/navigation.el
+++ customizations/navigation.el
@@ -36,7 +36,27 @@ point reaches the beginning or end of the buffer, stop there."
 
 (use-package helm
   :config
-  (require 'helm-config))
+  (require 'helm-config)
+  (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+        helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+        helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+        helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+        helm-ff-file-name-history-use-recentf t)
+  (helm-mode 1)
+  (global-unset-key (kbd "C-x c"))
+  :bind
+  (("C-c h" . helm-command-prefix)
+   ("M-x" . helm-M-x)
+   ("C-x b" . helm-buffers-list)
+   ("C-x C-f" . helm-find-files)
+   ("C-c p s t" . helm-ag-this-file)
+   :map helm-map
+   ;; rebind tab to run persistent action
+   ("<tab>" . helm-execute-persistent-action)
+   ;; make TAB works in terminal
+   ("C-i" . helm-execute-persistent-action)
+   ;; list actions using C-z
+   ("C-z" . helm-select-action)))
 
 (use-package helm-projectile
   :requires (helm projectile)
@@ -54,28 +74,6 @@ point reaches the beginning or end of the buffer, stop there."
 ;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
 ;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
 ;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
-(global-set-key (kbd "C-c h") 'helm-command-prefix)
-(global-unset-key (kbd "C-x c"))
-
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x b") 'helm-buffers-list)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "C-c p s t") 'helm-ag-this-file)
-
-;; rebind tab to run persistent action
-(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
-;; make TAB works in terminal
-(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
-;; list actions using C-z
-(define-key helm-map (kbd "C-z")  'helm-select-action)
-
-(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
-      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
-      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
-      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
-      helm-ff-file-name-history-use-recentf t)
-
-(helm-mode 1)
 
 (require 'zoom-window)
 (global-set-key (kbd "C-x C-z") 'zoom-window-zoom)
