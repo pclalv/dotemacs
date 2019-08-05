02b06435fa4e353b36f6e8e7cf582481247b31c5
paul alvarez <palvarez@greenhouse.io>
2019-08-05 19:02:55 -0400

    Meta use-package

--- customizations/setup-eglot.el
+++ customizations/setup-eglot.el
@@ -1,22 +1,19 @@
 (use-package eglot
+  :after projectile
   :bind (:map eglot-mode-map
-         ("C-c e a" . eglot-code-actions)
-         ("C-c e f" . eglot-format)
-         ("C-c e h" . eglot-help-at-point)
-         ("C-c e r" . eglot-rename)
-         ("C-c h" . eglot-help-at-point)))
+              ("C-c e a" . eglot-code-actions)
+              ("C-c e f" . eglot-format)
+              ("C-c e h" . eglot-help-at-point)
+              ("C-c e r" . eglot-rename)
+              ("C-c h" . eglot-help-at-point))
+  :config
+  ;; Bridge projectile and project together so packages that depend on
+  ;; project like eglot work
+  ;; https://github.com/joaotavora/eglot/issues/129#issuecomment-444130367
+  (defun my-projectile-project-find-function (dir)
+    (let ((root (projectile-project-root dir)))
+      (and root (cons 'transient root))))
+  (add-to-list 'project-find-functions 'my-projectile-project-find-function))
 
 ;; this didn't work; cryptic error message
 ;; (add-to-list 'eglot-server-programs '(clojure-mode . ("/Users/paulalvarez/bin/clojure-lsp")))
-
-;; Bridge projectile and project together so packages that depend on
-;; project like eglot work
-;; https://github.com/joaotavora/eglot/issues/129#issuecomment-444130367
-(defun my-projectile-project-find-function (dir)
-  (let ((root (projectile-project-root dir)))
-    (and root (cons 'transient root))))
-
-(projectile-mode t)
-
-(with-eval-after-load 'project
-  (add-to-list 'project-find-functions 'my-projectile-project-find-function))
