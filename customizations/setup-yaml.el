11ed8765e4184477fcf7be31802098da5aeecb4c
paul alvarez <palvarez@greenhouse.io>
2019-08-05 19:15:50 -0400

    use-package yaml-mode

--- customizations/setup-yaml.el
+++ customizations/setup-yaml.el
@@ -1,8 +1,7 @@
-(require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
-
-(add-hook 'yaml-mode-hook
-	  (lambda ()
-            (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
-
-(add-hook 'yaml-mode-hook 'highlight-indentation-mode)
+(use-package yaml-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+  (add-hook 'yaml-mode-hook
+            (lambda ()
+              (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+  (add-hook 'yaml-mode-hook 'highlight-indentation-mode))
