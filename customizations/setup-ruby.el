524d045e94b7ee1a892d5b53b3ae2234f0957ee1
paul alvarez <palvarez@greenhouse.io>
2019-08-05 19:25:36 -0400

    Simplify setup-ruby.el

--- customizations/setup-ruby.el
+++ customizations/setup-ruby.el
@@ -1,23 +1,22 @@
-;; none of this works??
-(add-hook 'ruby-mode-hook
-  (lambda () (hs-minor-mode)))
+(use-package ruby-mode)
 
-(eval-after-load "hideshow"
-  '(add-to-list 'hs-special-modes-alist
+(use-package highlight-indentation
+  :hook (ruby-mode . highlight-indentation-mode))
+
+(use-package electric
+  :hook (ruby-mode . electric-pair-mode))
+
+(use-package hideshow
+  :after (ruby-mode)
+  :bind
+  ;; hideshow bindings suck, but this doesn't work
+  (("C-c h" . hs-hide-block)
+   ("C-c s" . hs-show-block))
+  :config
+  (add-to-list 'hs-special-modes-alist
     `(ruby-mode
       ,(rx (or "def" "class" "module" "do" "{" "[")) ; Block start
       ,(rx (or "}" "]" "end"))                       ; Block end
       ,(rx (or "#" "=begin"))                        ; Comment start
-      ruby-forward-sexp nil)))
-
-;; enh-ruby-mode is slow ... but we'll give it another shot.
-;; (add-to-list 'auto-mode-alist
-;;              '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
-;; (setq enh-ruby-deep-indent-paren nil)
-
-(global-set-key (kbd "C-c h") 'hs-hide-block)
-(global-set-key (kbd "C-c s") 'hs-show-block)
-
-(add-hook 'ruby-mode-hook 'highlight-indentation-mode)
-(add-hook 'ruby-mode-hook 'electric-pair-mode)
-(add-hook 'ruby-mode-hook 'eglot-ensure)
+      ruby-forward-sexp nil))
+  :hook (ruby-mode . (lambda () (hs-minor-mode))))
