14efa7bd0d8484ab5f47fd5f38f15c17144429f8
paul alvarez <palvarez@greenhouse.io>
2019-09-04 16:33:59 -0400

    Cider history pls

--- init.el
+++ init.el
@@ -359,6 +359,7 @@ point reaches the beginning or end of the buffer, stop there."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cider-repl-history-file "~/.cider-history.eld")
  '(inf-ruby-default-implementation "pry")
  '(magit-log-section-arguments (quote ("--decorate" "-n256")))
  '(ruby-deep-indent-paren nil)
