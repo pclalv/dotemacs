be283cc128035f9b637ee77bf358346e54469142
paul alvarez <palvarez@greenhouse.io>
2019-12-14 10:18:20 -0500

    Try again to manage backup files

--- customizations/misc.el
+++ customizations/misc.el
@@ -1,14 +1,16 @@
 (add-to-list 'backup-directory-alist '("." . "~/.emacs.d/backups"))
 
-(setq backup-directory-alist
-      '(("." . "/tmp/emacs-backups")))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
+;; (setq auto-save-file-name-transforms
+;;       `((".*" ,temporary-file-directory t)))
 
-(setq delete-old-versions t
-  kept-new-versions 6
-  kept-old-versions 2
-  version-control t)
+;; source: https://www.emacswiki.org/emacs/BackupDirectory#toc2
+(setq backup-by-copying t      ; don't clobber symlinks
+      backup-directory-alist
+      '(("." . "~/.saves/"))    ; don't litter my fs tree
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)       ; use versioned backups
 
 (global-auto-revert-mode t)
 
