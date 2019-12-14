0f5e6f3e6064da56fbebead1fce573ba7ff682c1
paul alvarez <palvarez@greenhouse.io>
2019-12-14 10:47:44 -0500

    Add this empty thing

--- init.el
+++ init.el
@@ -385,3 +385,9 @@ point reaches the beginning or end of the buffer, stop there."
  '(inf-ruby-default-implementation "pry")
  '(magit-log-section-arguments (quote ("--decorate" "-n256")))
  '(ruby-deep-indent-paren nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
