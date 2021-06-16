21a74b69d7b7b778ba55057a93b8acb1e63513df
paul alvarez <pclalv@users.noreply.github.com>
2021-06-16 10:42:46 -0400

    format this block

--- init.el
+++ init.el
@@ -54,13 +54,12 @@
 ;; (setq auto-save-file-name-transforms
 ;;       `((".*" ,temporary-file-directory t)))
 ;; source: https://www.emacswiki.org/emacs/BackupDirectory#toc2
-(setq backup-by-copying t      ; don't clobber symlinks
-      backup-directory-alist
-      '(("." . "~/.saves/"))    ; don't litter my fs tree
+(setq backup-by-copying t ; don't clobber symlinks
+      backup-directory-alist '(("." . "~/.saves/")) ; don't litter my fs tree
       delete-old-versions t
       kept-new-versions 6
       kept-old-versions 2
-      version-control t)       ; use versioned backups
+      version-control t) ; use versioned backups
 
 (global-auto-revert-mode t)
 
