b2e06c92fb7d5ada4ea412b5af55ae1b6ae8158c
paul alvarez <pclalv@users.noreply.github.com>
2022-04-18 12:05:36 -0400

    roll navigation.el into init.el

--- init.el
+++ init.el
@@ -139,6 +139,66 @@ point reaches the beginning or end of the buffer, stop there."
 
 (straight-use-package 'use-package)
 
+(use-package projectile
+  :straight t
+  :demand t
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :config
+  (projectile-global-mode)
+  (setq projectile-switch-project-action 'projectile-vc)
+  (add-to-list 'projectile-globally-ignored-directories "log")
+  (add-to-list 'projectile-globally-ignored-directories "tmp")
+  (add-to-list 'projectile-globally-ignored-directories "vendor"))
+
+;; consider using selectrum and prescient instead, now that helm has been ceased development
+(use-package helm
+  :straight t
+  :demand t
+  :config
+  (require 'helm-config)
+  (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+        helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+        helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+        helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+        helm-ff-file-name-history-use-recentf t)
+  (helm-mode 1)
+  ;; The default `helm-command-prefix' "C-x c" is quite close to "C-x
+  ;; C-c", which quits Emacs.
+  :bind
+  (("M-x" . helm-M-x)
+   ("C-x b" . helm-buffers-list)
+   ("C-x C-f" . helm-find-files)
+   :map helm-map
+   ("<tab>" . helm-execute-persistent-action) ;; rebind tab to run persistent action
+   ("C-i" . helm-execute-persistent-action) ;; make TAB works in terminal
+   ("C-z" . helm-select-action))) ;; list actions using C-z
+   
+
+(use-package helm-projectile
+  :straight t
+  :demand t
+  :requires (helm projectile)
+  :bind (:map helm-command-map ("s" . helm-ag-this-file))
+  :config
+  (helm-projectile-on)
+  (setq projectile-completion-system 'helm))
+
+(use-package helm-rg
+  :straight t
+  :requires helm)
+
+(use-package helm-ag
+  :straight t
+  :requires helm
+  :config
+  (add-to-list 'grep-find-ignored-directories "log")
+  (add-to-list 'grep-find-ignored-directories "tmp")
+  (add-to-list 'grep-find-ignored-directories "vendor")
+  (add-to-list 'grep-find-ignored-directories "coverage")
+  (add-to-list 'grep-find-ignored-directories "_build")
+  (add-to-list 'grep-find-ignored-directories "node_modules"))
+
 (use-package ace-window
   :straight t
   :bind ("C-x o" . ace-window))
@@ -394,10 +454,23 @@ point reaches the beginning or end of the buffer, stop there."
 (use-package z80-mode
   :load-path "vendor/")
 
+(use-package typescript-mode
+  :straight t)
+
 (use-package js
   :config
   (setq js-indent-level 2))
 
+(use-package rjsx-mode
+  :straight t)
+
+(use-package tide
+  :straight t
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)
+         (before-save . tide-format-before-save)))
+;; TODO: look into something like this https://dev.to/viglioni/how-i-set-up-my-emacs-for-typescript-3eeh
 
 (add-to-list 'load-path "~/.emacs.d/vendor")
 
@@ -413,10 +486,16 @@ point reaches the beginning or end of the buffer, stop there."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/Dropbox (Personal)/habits.org"))
+ '(org-agenda-files
+   '("~/Desktop/todo.org" "/Users/palvarez/Dropbox (Personal)/habits.org"))
  '(org-modules
    '(org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m)))
 (custom-set-faces)
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
