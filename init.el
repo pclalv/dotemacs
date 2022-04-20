9a5824042d3f315b0cf1b49deb8599e6bba66449
paul alvarez <pclalv@users.noreply.github.com>
2022-04-20 13:42:04 -0400

    Remove customizations

--- init.el
+++ init.el
@@ -253,6 +253,11 @@ point reaches the beginning or end of the buffer, stop there."
   :straight t
   :hook (ruby-mode . highlight-indentation-mode))
 
+(use-package org
+  :custom
+  (org-modules
+   '(org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m)))
+
 (use-package magit
   :straight t
   :bind ("C-x g" . magit-status)
@@ -434,29 +439,3 @@ point reaches the beginning or end of the buffer, stop there."
 (load "setup-ocaml.el")
 (load "setup-org.el")
 (load "setup-smalltalk.el")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   '("~/Desktop/todo.org" "/Users/palvarez/Dropbox (Personal)/habits.org"))
- '(org-modules
-   '(org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m)))
-(custom-set-faces)
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- 
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- 
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- 
