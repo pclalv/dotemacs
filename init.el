45b9cb87acbc2f9e1b8e1b4671d9b5f08e5de8fc
paul alvarez <pclalv@users.noreply.github.com>
2024-10-04 14:46:06 -0700

    add nix-mode

--- init.el
+++ init.el
@@ -484,6 +484,10 @@ point reaches the beginning or end of the buffer, stop there."
 (use-package puppet-mode
   :straight t)
 
+(use-package nix-mode
+  :straight t
+  :mode "\\.nix\\'")
+
 (when (and (eq system-type 'gnu/linux)
            (getenv "WSLENV"))
   (global-set-key (kbd "M-<f6>") 'just-one-space) ; hack because Windows hijacks Alt+space
